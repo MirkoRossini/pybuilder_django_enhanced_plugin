13e411ac03ab41ded7bf391af8724dc407a35b19
Mirko Rossini <mirko.rossini@ymail.com>
2014-09-28 23:35:14 -0400

    Added README and MANIFEST file

---
+++ README.rst
@@ -0,0 +1,21 @@
+PyBuilder Django Enhanced Plugin 
+================================
+
+Plugin for pybuilder providing some useful tasks for django development
+
+How to use pybuilder_django_enhanced_plugin
+----------------------------------
+
+Add plugin dependency to your `build.py`
+```python
+use_plugin('pypi:pybuilder_django_enhanced_plugin')
+```
+
+Configure the plugin within your `init` function:
+```python
+@init
+def init(project):
+    project.set_property('django_module', 'myproject')
+    project.set_property('django_apps', ['myapp', 'myotherapp'])
+    project.set_property('django_subpath', 'nested/dir')
+```
--- build.py
+++ build.py
@@ -5,6 +5,7 @@ use_plugin("python.integrationtest")
 use_plugin("python.install_dependencies")
 use_plugin("python.flake8")
 use_plugin("python.coverage")
+use_plugin("copy_resources")
 use_plugin("python.distutils")
 
 
@@ -28,6 +29,29 @@ def set_properties(project):
     project.build_depends_on("flake8")
     project.set_property('integrationtest_inherit_environment', True)
     project.set_property('coverage_break_build', False)
+
+    project.set_property("copy_resources_target", "$dir_dist")
     project.get_property("copy_resources_glob").append("LICENSE")
+    project.get_property("copy_resources_glob").append("MANIFEST.in")
+    project.get_property("copy_resources_glob").append("README.rst")
+    # project.get_property("distutils_commands").append("bdist_wheel")
+    # project.set_property("distutils_console_scripts", ["pyb_ = pybuilder.cli:main"])
+    project.set_property("distutils_classifiers", [
+                         'Programming Language :: Python',
+                         'Programming Language :: Python :: Implementation :: CPython',
+                         'Programming Language :: Python :: Implementation :: PyPy',
+                         'Programming Language :: Python :: 2.6',
+                         'Programming Language :: Python :: 2.7',
+                         'Programming Language :: Python :: 3',
+                         'Programming Language :: Python :: 3.2',
+                         'Programming Language :: Python :: 3.3',
+                         'Programming Language :: Python :: 3.4',
+                         'Development Status :: 4 - Beta',
+                         'Environment :: Console',
+                         'Intended Audience :: Developers',
+                         'License :: OSI Approved :: BSD Software License',
+                         'Topic :: Software Development :: Build Tools',
+                         'Topic :: Software Development :: Quality Assurance',
+                         'Topic :: Software Development :: Testing'])
 
 
